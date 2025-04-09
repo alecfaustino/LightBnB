be5d6e3f66ad5e13260dfd9ceee6b85ad393e047
Alec Faustino <alecfaustino@outlook.com>
2025-04-08 20:45:31 -0600

    add table statements for properties and reservations

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -8,3 +8,29 @@ CREATE TABLE users (
 );
 
 
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) NOT NULL, 
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night INTEGER NOT NULL,
+  parking_spaces INTEGER NOT NULL,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
+  active BOOLEAN NOT NULL
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) NOT NULL,
+  guest_id INTEGER REFERENCES users(id) NOT NULL,
+)
