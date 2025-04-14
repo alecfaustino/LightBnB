6b3303decc60a356b95c5c1e1d1da1ae295bc5cb
Alec Faustino <alecfaustino@outlook.com>
2025-04-13 18:00:01 -0600

    configure pool to connect to proper database

--- LightBnB_WebApp/db/index.js
+++ LightBnB_WebApp/db/index.js
@@ -1,6 +1,11 @@
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const pool = new Pool({
+  user: 'development',
+  password: 'development',
+  host: 'localhost',
+  database: 'lightbnb',
+});
 
 const query = (text, params, callback) => {
   return pool.query(text, params, callback);
